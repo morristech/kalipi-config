c863b40c8e57f0f86329bb67cd63dd951554985c
SwiftHackerz <33038633+SwiftHackerz@users.noreply.github.com>
2018-07-18 20:04:59 -0700

    Delete kalipi.sh

--- kalipi.sh
+++
@@ -1,4 +0,0 @@
-#Sudo if needed
-sudo apt install whiptail parted lua5.1 alsa-utils psmisc
-sudo wget -O /usr/local/bin/kalipi-config https://raw.githubusercontent.com/Re4son/RPi-Tweaks/master/kalipi-config/kalipi-config
-sudo chmod o+x /usr/local/bin/kalipi-config
