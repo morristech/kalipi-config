20f00adfe6012f1736d47bde5e86032ffa392058
SwiftHackerz <33038633+SwiftHackerz@users.noreply.github.com>
2018-07-20 12:15:18 +1000

    Changed any errors and typos

--- kalipi.sh
+++ kalipi.sh
@@ -1,6 +1,6 @@
 #Sudo if needed
 sudo apt-get install whiptail parted lua5.1 alsa-utils psmisc -y
 sudo wget -O /usr/local/bin/kalipi-config https://raw.githubusercontent.com/Re4son/RPi-Tweaks/master/kalipi-config/kalipi-config
-sudo chmod o+x /usr/local/bin/kalipi-config
-echo Finished!
-echo Use sudo kalipi-config to start it up. :)
+sudo chmod 755 /usr/local/bin/kalipi-config
+echo Finished...
+echo Use sudo kalipi-config to start it up. 
